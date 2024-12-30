0edc882b8c2f72429168b6a22cba61208dd79695
David McNett <contact@nugget.info>
2024-12-29 22:08:59 -0600

    Further refinement and commenting

--- examples/vehiclestats/vehiclestats.go
+++ examples/vehiclestats/vehiclestats.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"io"
 	"log/slog"
 	"os"
 
@@ -14,7 +16,9 @@ var (
 	logLevel *slog.LevelVar
 )
 
-func setupLogs() {
+// setupLogs populates the global logger and logLevel variables. It
+// also chooses an appropriate log level based on runtime attributes.
+func setupLogs(verboseLogs *bool) {
 	logLevel = new(slog.LevelVar)
 	logLevel.Set(slog.LevelInfo)
 
@@ -26,52 +30,81 @@ func setupLogs() {
 
 	slog.SetDefault(logger)
 	slog.SetLogLoggerLevel(slog.LevelInfo)
-}
 
-func main() {
-	setupLogs()
-
-	var debugMode = flag.Bool("v", false, "Verbose logging")
-	var filename = flag.String("file", "", "Road Trip vehicle CSV file")
-
-	flag.Parse()
-
-	if *filename == "" {
-		logger.Error("no filename provided (--file)")
-		os.Exit(1)
-	}
-
-	options := roadtrip.VehicleOptions{
-		Logger: logger,
-	}
-
-	if *debugMode {
+	if *verboseLogs {
 		// AddSource: true here
 		slog.SetLogLoggerLevel(slog.LevelDebug)
 		logLevel.Set(slog.LevelDebug)
 	}
 
+	return
+}
+
+// run is the real main, but one where we can exit with an error.
+func run(
+	ctx context.Context,
+	stdout io.Writer,
+	getenv func(string) string,
+	args []string,
+) error {
+	myFlags := flag.NewFlagSet("myFlags", flag.ExitOnError)
+
+	var verboseLogs = myFlags.Bool("v", false, "Verbose logging")
+	var filename = myFlags.String("file", "", "Road Trip vehicle CSV file")
+
+	err := myFlags.Parse(args[1:])
+	if err != nil {
+		return err
+	}
+	args = myFlags.Args()
+
+	setupLogs(verboseLogs)
+
+	if *filename == "" {
+		return fmt.Errorf("no filename provided (--file)")
+	}
+
+	//
+	// Here's where the interesting stuff starts as far as this library is concerned.
+	//
+
+	// The roadtrip package will happily use your log/slog Logger if you have one.
+	options := roadtrip.VehicleOptions{Logger: logger}
+
 	// Create a [roadtrip.Vehicle] object with contents from a Road Trip data file.
 	vehicle, err := roadtrip.NewVehicleFromFile(*filename, options)
 	if err != nil {
-		logger.Error("unable to load Road Trip data file",
-			"error", err,
-		)
-		os.Exit(1)
+		return err
 	}
 
 	logger.Info("Loaded vehicle", "vehicle", vehicle)
 
+	// Let's caclulate how much we've spent on fuel so far, starting at 0.
 	totalFuelCost := 0.00
 
 	for i, f := range vehicle.FuelRecords {
-		logger.Debug("Fuel Record",
-			"index", i,
-			"fuel", f,
-		)
+		logger.Debug("Fuel Record", "index", i, "fuel", f)
+
 		totalFuelCost += f.TotalPrice
 	}
 
+	fmt.Printf("-- \n\n")
 	fmt.Printf("%s\n", vehicle.Vehicles[0].Name)
 	fmt.Printf("Spent %0.02f on fuel in %d fillups\n", totalFuelCost, len(vehicle.FuelRecords))
+
+	return nil
+}
+
+// main does as little as we can get away with.
+func main() {
+	ctx := context.Background()
+	if err := run(
+		ctx,
+		os.Stdout,
+		os.Getenv,
+		os.Args,
+	); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
 }
